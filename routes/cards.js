e77b19f9e1f389e1ae9b665005a69f20dd89d1aa
Spynu Jean <jspynu14@mail.ru>
2022-04-22 17:30:16 +0400

    project14

--- routes/cards.js
+++ routes/cards.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 const auth = require('../middlewares/auth');
 
+const {
+  createCardValid,
+  parameterIdValid,
+} = require('../middlewares/validation');
+
 const
   {
     createCard,
@@ -12,9 +17,9 @@ const
   } = require('../controllers/cards');
 
 router.post('/', auth, createCard);
-router.get('/', auth, getCard);
-router.delete('/:cardid', auth, deleteCard);
-router.put('/:cardid/likes', auth, setCardLikes);
-router.delete('/:cardid/likes', auth, deleteCardLikes);
+router.get('/', auth, createCardValid, getCard);
+router.delete('/:cardid', parameterIdValid('cardid'), auth, deleteCard);
+router.put('/:cardid/likes', auth, parameterIdValid('cardid'), setCardLikes);
+router.delete('/:cardid/likes', auth, parameterIdValid('cardid'), deleteCardLikes);
 
 module.exports = router;
