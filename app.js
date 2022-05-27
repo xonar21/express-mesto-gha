3ac3a7663260c57a0b3c9b83448218bcfe293feb
Spynu Jean <jspynu14@mail.ru>
2022-05-27 18:15:04 +0400

    project15

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const { errors } = require('celebrate');
@@ -20,37 +22,7 @@ const { login, createUser } = require('./controllers/users');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
-
-// const allowedCors = [
-//   'https://mestoproject.nomoredomains.xyz',
-//   'http://mestoproject.nomoredomains.xyz',
-//   'http://localhost:3000'
-// ];
-
-// app.use(function(req, res, next) {
-//   const { origin } = req.headers;
-
-//   const { method } = req;
-
-//   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-
-//   const requestHeaders = req.headers['access-control-request-headers'];
-
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//   }
-
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-
-//   next();
-// });
+const { PORT = 4000 } = process.env;
 
 app.use(cors({
   origin: [
@@ -72,47 +44,6 @@ app.post('/signup', registerValid, createUser);
 app.post('/signin', loginValid, login);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-//   if (req.method === 'OPTIONS') {
-//     res.send(200);
-//   }
-//   next();
-// });
-
-// const allowedCors = [
-//   'https://mestoproject.nomoredomains.xyz',
-//   'http://mestoproject.nomoredomains.xyz',
-//   'http://localhost:3000'
-// ];
-
-// app.use(function(req, res, next) {
-//   const { origin } = req.headers;
-
-//   const { method } = req;
-
-//   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-
-//   const requestHeaders = req.headers['access-control-request-headers'];
-
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//   }
-
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//   }
-
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-
-//   next();
-// });
 
 app.use(auth);
 
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,21 @@
 const jwt = require('jsonwebtoken');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const Unauthorized = require('../errors/Unauthorized');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  // const { authorization } = req.headers;
+  const token = req.headers.authorization;
+  if (!token || !token.startsWith('Bearer ')) {
     return next(new Unauthorized('Необходима авторизация'));
   }
 
-  const token = String(req.headers.authorization).replace('Bearer ', '');
+  //const token = String(req.headers.authorization).replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new Unauthorized('Необходима авторизация'));
   }
