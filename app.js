3c464c450b75f7a58c6fb8f62b9966137df2edfe
Spynu Jean <jspynu14@mail.ru>
2022-05-27 18:22:42 +0400

    project15

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', registerValid, createUser);
 
 app.post('/signin', loginValid, login);
